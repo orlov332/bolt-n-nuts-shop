008648cdf5eeab901128a32b4fe66e709dcf91ef
orlov332 <orlov332@gmail.com>
2022-06-22 20:35:41 +0200

    refactor: Refactor query

--- product-service/src/repository/ProductRepository.ts
+++ product-service/src/repository/ProductRepository.ts
@@ -1,5 +1,14 @@
 import { Client } from 'pg';
 
+const ALL_PRODUCTS_QUERY =
+  `select p.id          as id,
+          p.title       as title,
+          p.description as description,
+          p.price       as price,
+          s.count       as count
+   from products p
+            left join stocks s on p.id = s.product_id`;
+
 export class ProductRepository {
 
   constructor(
@@ -8,7 +17,7 @@ export class ProductRepository {
   }
 
   getAll = async () =>
-    this.client.query('select * from products')
+    this.client.query(ALL_PRODUCTS_QUERY)
       .then(res => res.rows);
 
 }
