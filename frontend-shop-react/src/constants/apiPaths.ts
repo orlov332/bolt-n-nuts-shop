45166d243c20155212bade429d39ce20dab72cf6
orlov332 <orlov332@gmail.com>
2022-07-31 17:43:03 +0200

    feat: Integrate cart service with frontend

--- frontend-shop-react/src/constants/apiPaths.ts
+++ frontend-shop-react/src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   import: "https://zxcw28zxdb.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev"
+  cart: "http://bolt-n-nuts-cart-api-develop.eu-west-1.elasticbeanstalk.com/api"
 };
 
 export default API_PATHS;
